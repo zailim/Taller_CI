985b213789b2bda97c20d5426ca108cd9ccfd903
zaira <rosinzaira@gmail.com>
2025-06-08 23:08:37 -0300

    Checkpoint de estado seguro jaja

--- primos.py
+++ primos.py
@@ -1,6 +1,6 @@
 def es_primo(n):
     """Devuelve True si n es primo, False en caso contrario."""
-    if n > 1:
+    if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
