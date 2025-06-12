ded52447bd969d4ec63c9816496a7d9d25c29996
zaira <rosinzaira@gmail.com>
2025-06-12 03:47:48 -0300

    dejando todo 0km

--- primos.py
+++ primos.py
@@ -1,7 +1,7 @@
 def es_primo(n):
     """Devuelve True si n es primo, False en caso contrario."""
     if n <= 1:
-        return True
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
